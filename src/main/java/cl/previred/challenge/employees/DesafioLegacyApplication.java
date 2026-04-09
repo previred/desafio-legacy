9b784df602c8d0e7fa267101289cbd4794ab4cf8
Rodrigo Sepulveda <rodrisepulveda@gmail.com>
2026-04-09 17:40:50 -0400

    Commit inicial

---
+++ src/main/java/cl/previred/challenge/employees/DesafioLegacyApplication.java
@@ -0,0 +1,13 @@
+package cl.previred.challenge.employees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioLegacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioLegacyApplication.class, args);
+	}
+
+}
