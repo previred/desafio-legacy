c39b25d116e10377b6e3b90813baa77fea4e79d5
Carciola <andresavila.r@hotmail.com>
2026-04-13 19:11:12 -0400

    feat: estructura base y configuracion

---
+++ src/test/java/com/previred/desafiolegacy/DesafiolegacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.previred.desafiolegacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiolegacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
