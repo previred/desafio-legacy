98069239eeb189c5cb4c3c8beff9d928d70f4217
Carciola <andresavila.r@hotmail.com>
2026-04-14 20:51:37 -0400

    feat: validaciones reglas de negocio

---
+++ src/main/java/com/previred/desafiolegacy/validator/EmpleadoValidator.java
@@ -0,0 +1,50 @@
+package com.previred.desafiolegacy.validator;
+
+import com.previred.desafiolegacy.exception.ValidacionException;
+import com.previred.desafiolegacy.model.Empleado;
+import com.previred.desafiolegacy.repository.EmpleadoRepository;
+
+public class EmpleadoValidator {
+
+    private final EmpleadoRepository empleadoRepository;
+
+    public EmpleadoValidator(EmpleadoRepository empleadoRepository) {
+        this.empleadoRepository = empleadoRepository;
+    }
+
+    public void validar(Empleado empleado) {
+        validarRutDuplicado(empleado.getRut());
+        validarSalarioBase(empleado.getSalarioBase());
+        validarBono(empleado.getBono(), empleado.getSalarioBase());
+        validarDescuentos(empleado.getDescuentos(),  empleado.getSalarioBase());
+    }
+
+    private void validarRutDuplicado(String rut) {
+        if (empleadoRepository.existsByRut(rut)) {
+            throw new ValidacionException("rut",
+                    "Ya existe un empleado con el RUT: " + rut);
+        }
+    }
+
+    private void validarSalarioBase(double salarioBase) {
+        if (salarioBase < 400000) {
+            throw new ValidacionException("salarioBase",
+                    "El salario base no puede ser menor a $400.000");
+        }
+    }
+
+    private void validarBono(double bono, double salarioBase) {
+        if (bono > salarioBase * 0.5) {
+            throw new ValidacionException("bono",
+                    "El bono no puede superar el 50% del salario base");
+        }
+    }
+
+    private void validarDescuentos(double descuentos, double salarioBase) {
+        if (descuentos > salarioBase) {
+            throw new ValidacionException("descuentos",
+                    "El total de descuentos no puede ser mayor al salario base");
+        }
+    }
+
+}
